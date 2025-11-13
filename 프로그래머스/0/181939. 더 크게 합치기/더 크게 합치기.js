9851a73042dff61a44ad120d755351edf6327092
조혜주 <srose1225@naver.com>
2025-11-14 02:09:06 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    const result1 = String(a) + String(b);
+    const result2 = String(b) + String(a);
+    
+    if (result1 >= result2) {
+        return Number(result1);
+    } else {
+        return Number(result2);
+    }
+}
