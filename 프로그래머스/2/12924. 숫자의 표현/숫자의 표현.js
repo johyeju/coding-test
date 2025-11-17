7db01221e6eaf20a3bb8a4e86ac8c2ee4d580935
조혜주 <srose1225@naver.com>
2025-11-17 20:38:30 +0900

    [level 2] Title: 숫자의 표현, Time: 1.51 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let answer = 0;
+    let start = 1;
+    let end = 1;
+    let sum = 1;
+    
+    while (start <= n) {
+        if (sum === n) {
+            answer++;
+            sum -= start;
+            start++;
+        } else if (sum < n) {
+            end++;
+            sum += end;
+        } else { // sum > n
+            sum -= start;
+            start++;
+        }
+    }
+    
+    return answer;
+}
